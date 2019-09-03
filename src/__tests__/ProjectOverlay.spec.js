3b73da1f159048870f2ea562e63886b8bc80a6e6
Christy Jacob <christyjacob4@gmail.com>
2019-09-03 22:10:05 +0530

    feat: added tests for ProjectOverlay.js

---
+++ src/__tests__/ProjectOverlay.spec.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { ProjectOverlay } from '../components/ProjectOverlay';
+import { useProjectsValue } from '../context';
+
+beforeEach(cleanup); // thanks!
+
+jest.mock('../context', () => ({
+  useProjectsValue: jest.fn(() => ({
+    projects: [
+      {
+        name: '🙌 THE OFFICE',
+        projectId: '1',
+        userId: 'jlIFXIwyAL3tzHMtzRbw',
+        docId: 'michael-scott',
+      },
+    ],
+  })),
+}));
+
+describe('<ProjectOverlay', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('Success', () => {
+    it('renders the project overlay and calls setShowProjectOverlay using onClick', () => {
+      const showProjectOverlay = true;
+      const setProject = jest.fn();
+      const setShowProjectOverlay = jest.fn(() => !showProjectOverlay);
+
+      const { queryByTestId } = render(
+        <ProjectOverlay
+          showProjectOverlay
+          setProject={setProject}
+          setShowProjectOverlay={setShowProjectOverlay}
+        />
+      );
+
+      expect(queryByTestId('project-overlay')).toBeTruthy();
+      fireEvent.click(queryByTestId('project-overlay-action'));
+      expect(setProject).toHaveBeenCalled();
+    });
+
+    it('renders the project overlay and calls setShowProjectOverlay using onKeyDown', () => {
+      const showProjectOverlay = true;
+      const setProject = jest.fn();
+      const setShowProjectOverlay = jest.fn(() => !showProjectOverlay);
+
+      const { queryByTestId } = render(
+        <ProjectOverlay
+          showProjectOverlay
+          setProject={setProject}
+          setShowProjectOverlay={setShowProjectOverlay}
+        />
+      );
+
+      expect(queryByTestId('project-overlay')).toBeTruthy();
+      fireEvent.keyDown(queryByTestId('project-overlay-action'));
+      expect(setProject).toHaveBeenCalled();
+    });
+  });
+
+  describe('Failure', () => {
+    it('does not render the project overlay with any projects', () => {
+      useProjectsValue.mockImplementation(() => ({
+        projects: [],
+      }));
+
+      const { queryByTestId } = render(<ProjectOverlay showProjectOverlay />);
+      expect(queryByTestId('project-overlay')).toBeTruthy();
+      expect(queryByTestId('project-overlay-action')).toBeFalsy();
+    });
+  });
+});
