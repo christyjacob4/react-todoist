77bfe81895c0eb710bf06ed39898bd699c7b458f
Christy Jacob <christyjacob4@gmail.com>
2019-09-03 21:59:53 +0530

    feat: added content component

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Sidebar} from './Sidebar';
-import {Tasks} from '../Tasks';
+import { Sidebar } from './Sidebar';
+import { Tasks } from '../Tasks';
 
 export const Content = () => (
-  <section>
-    <Sidebar/>
+  <section className="content">
+    <Sidebar />
     <Tasks />
   </section>
 );
