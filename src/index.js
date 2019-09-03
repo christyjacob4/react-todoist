959554b26f1a578f777d3855d8c7057aafa67b47
Christy Jacob <christyjacob4@gmail.com>
2019-09-03 21:53:19 +0530

    Added index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { App } from './App';
 import './App.scss';
 
-render(<App />, document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
