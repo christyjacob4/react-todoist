247bdb957b57a472582a260fdb6e4652b7f8ae25
Christy Jacob <christyjacob4@gmail.com>
2019-09-03 21:50:02 +0530

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import {Header} from './components/layout/Header';
-import {Content} from './components/layout/Content';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
 
-export const App = () => (
-  <div className="App">
-    <Header/>
-    <Content/>
-  </div>
-);
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
 
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <main
+          data-testid="application"
+          className={darkMode ? 'darkmode' : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
+
+App.propTypes = {
+  darkModeDefault: PropTypes.bool,
+};
