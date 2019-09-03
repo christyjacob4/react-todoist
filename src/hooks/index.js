20d5bbe6546f50e727cc284de7f99b1580a27218
Christy Jacob <christyjacob4@gmail.com>
2019-09-03 21:54:02 +0530

    Added Hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
-import { firebase } from "../firebase";
-import { collatedTaskExists } from "../helpers";
-import moment from "moment";
+/* eslint-disable no-nested-ternary */
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import { firebase } from '../firebase';
+import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]);
@@ -10,65 +11,60 @@ export const useTasks = selectedProject => {
   useEffect(() => {
     let unsubscribe = firebase
       .firestore()
-      .collection("tasks")
-      .where("userId", "==", "hYGihujhgikGuiGKjh");
+      .collection('tasks')
+      .where('userId', '==', 'jlIFXIwyAL3tzHMtzRbw');
 
     unsubscribe =
-      selectedProject && !collatedTaskExists(selectedProject)
-        ? (unsubscribe = unsubscribe.where("projectId", "==", selectedProject))
-        : selectedProject === "TODAY"
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
         ? (unsubscribe = unsubscribe.where(
-            "date",
-            "==",
-            moment().format("DD/MM/YYYY")
+            'date',
+            '==',
+            moment().format('DD/MM/YYYY')
           ))
-        : selectedProject === "INBOX" || selectedProject === 0
-        ? (unsubscribe = unsubscribe.where("date", "==", ""))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe;
 
     unsubscribe = unsubscribe.onSnapshot(snapshot => {
-      const newTask = snapshot.docs.map(task => ({
+      const newTasks = snapshot.docs.map(task => ({
         id: task.id,
-        ...task.data()
+        ...task.data(),
       }));
 
-      // If you've selected NEXT7 as the project, filter those tasks that are older than 7 days and
-      // are archived
       setTasks(
-        selectedProject === "NEXT_7"
-          ? newTask.filter(
+        selectedProject === 'NEXT_7'
+          ? newTasks.filter(
               task =>
-                moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
+                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
                 task.archived !== true
             )
-          : newTask.filter(task => task.archived !== true)
+          : newTasks.filter(task => task.archived !== true)
       );
-
-      setArchivedTasks(newTask.filter(task => task.archived !== false));
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
     });
 
     return () => unsubscribe();
   }, [selectedProject]);
-  // What this means is that when selectedProject changes, re-run all of this.
 
   return { tasks, archivedTasks };
 };
 
-export const useProjecs = () => {
-  // React Hook to update projects by calling the setProjects
+export const useProjects = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     firebase
       .firestore()
-      .collection("projects")
-      .where("userId", "==", "hYGihujhgikGuiGKjh")
-      .orderBy("projectId")
+      .collection('projects')
+      .where('userId', '==', 'jlIFXIwyAL3tzHMtzRbw')
+      .orderBy('projectId')
       .get()
       .then(snapshot => {
         const allProjects = snapshot.docs.map(project => ({
           ...project.data(),
-          docId: project.id
+          docId: project.id,
         }));
 
         if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
